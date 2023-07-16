6953edeacf1c24d0e3869f459acf5fde96ac8c45
Svetlana Kozyreva <lana@cunamiweb.com>
2023-07-16 22:21:49 +0400

    fix icons

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,15 @@ let mix = require('laravel-mix');
 mix.copy('resources/admin/images', 'public/assets/admin/images');
 mix.copy('resources/admin/fonts', 'public/assets/admin/fonts');
 
-mix.sass('resources/admin/css/app.scss', 'public/assets/admin/css/app.css')
+mix.sass('resources/admin/css/app.scss', 'public/assets/admin/css/build.css')
     .js('resources/admin/js/app.js', 'public/assets/admin/js/app.js')
     .sourceMaps()
     .webpackConfig({devtool: 'inline-source-map'})
     .version();
+
+mix.styles([
+        'public/assets/admin/css/build.css',
+        'public/assets/admin/css/vendor/icons.css',
+    ],
+    'public/assets/admin/css/app.css'
+);
