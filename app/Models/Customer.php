cc54f2ff330d4d566058c7c672ce024942eb158b
Bohdan Padalko <thepinepain@gmail.com>
2023-07-20 12:47:16 +0200

    Generate ide helpers and model dockblocks

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,6 +6,34 @@ use App\Enums\CustomerSexEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Customer
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $first_name
+ * @property string $last_name
+ * @property CustomerSexEnum|null $sex
+ * @property \Illuminate\Support\Carbon|null $birth_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read string $full_name
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\CustomerGroup> $groups
+ * @property-read int|null $groups_count
+ * @method static \Database\Factories\CustomerFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereBirthDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereSex($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
     use HasFactory;
--- app/Models/CustomerGroup.php
+++ app/Models/CustomerGroup.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\CustomerGroup
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Customer> $customers
+ * @property-read int|null $customers_count
+ * @method static \Database\Factories\CustomerGroupFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup query()
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|CustomerGroup whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class CustomerGroup extends Model
 {
     use HasFactory;
